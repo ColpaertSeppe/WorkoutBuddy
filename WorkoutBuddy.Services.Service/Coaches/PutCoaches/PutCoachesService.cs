44ab8844c7328b1cc4a191897da86b6e3f6c2d5d
Luna <luna.devuyst@student.hogent.be>
2022-03-17 14:52:53 +0100

    Add File structure to projects for Coaches

---
+++ WorkoutBuddy.Services.Service/Coaches/PutCoaches/PutCoachesService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutBuddy.Services.Service.Coaches.PutCoaches
+{
+    class PutCoachesService : IPutCoachesService
+    {
+        public void PUT()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WorkoutBuddy.WebApi/Controllers/Coaches/GetCoaches/GetCoachesMapping.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkoutBuddy.WebApi.Controllers.Coaches.GetCoaches
+{
+    public class GetCoachesMapping
+    {
+    }
+}
